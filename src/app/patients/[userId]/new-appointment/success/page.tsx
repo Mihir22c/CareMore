5a8e5ef42bf98e2cddcf0e2f83e2332a0934c725
mihir22c <mihirbavishi111@gmail.com>
2025-01-24 18:26:21 +0100

    Add appointment fetching functionality and enhance success page with appointment details

--- src/app/patients/[userId]/new-appointment/success/page.tsx
+++ src/app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,8 +1,17 @@
+import { getAppointment } from '@/app/lib/actions/appointment.action'
+import { formatDateTime } from '@/app/lib/utils'
+import { Button } from '@/components/ui/button'
+import { Doctors } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const SuccessPage = () => {
+const SuccessPage = async ({ params: { userId }, searchParams }: SearchParamProps) => {
+
+    const appointmentId = (searchParams?.appointmentId as string) || ''
+    const appointment = await getAppointment(appointmentId)
+    const doctor = Doctors.find((doc) => doc.name === appointment.primaryPhysician)
+
     return (
         <div className='flex h-screen max-h-screen px-[5%]'>
             <div className='success-img'>
@@ -32,9 +41,36 @@ const SuccessPage = () => {
                 <section className='request-details'>
                     <p>Requested appointment details:</p>
                     <div className='flex items-center gap-3'>
+                        <Image
+                            src={doctor?.image}
+                            alt='doctor'
+                            height={100}
+                            width={100}
+                            className='size-6'
+                        />
+                        <p className='whitespace-nowrap'>Dr. {doctor?.name}</p>
+                    </div>
 
+                    <div className='flex gap-2'>
+                        <Image
+                            src='/assets/icons/calendar.svg'
+                            alt='calendar'
+                            height={24}
+                            width={24}
+                        />
+                        <p>{formatDateTime(appointment.schedule).dateTime}</p>
                     </div>
                 </section>
+
+                <Button variant={'outline'} className='shad-primary-btn'>
+                    <Link href={`/patients/${userId}/new-appointment`}>
+                        New Appointment
+                    </Link>
+                </Button>
+
+                <p className="copyright">
+                    © 2025 CarePulse
+                </p>
             </div>
         </div>
     )
