4fcd42ecbd446554c75e0b27b476c55d2f2f117e
mihir22c <mihirbavishi111@gmail.com>
2025-01-27 18:20:41 +0100

    Add Passkey verification modal and OTP input components for admin access

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModel from "@/components/PasskeyModel";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+
+  const isAdmin = searchParams.admin === "true";
   return (
     <div className="flex h-screen max-h-screen">
       {/* TODO: OTP Verification */}
+      {isAdmin && <PasskeyModel />}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -23,7 +28,7 @@ export default function Home() {
             <p className="justify-items-end text-dark-600 xl:text-left">
               © 2025 CarePulse
             </p>
-            <Link href="/?admin-true" className="text-green-500">
+            <Link href="/?admin=true" className="text-green-500">
               Admin
             </Link>
           </div>
